cc83e6b38205e41671a041573f0638b230d52855
Seth Shelnutt <Shelnutt2@gmail.com>
2021-09-06 09:35:53 -0400

    Swap all internal usage of FragmentMeta* for tdb_shared_ptr<FragmentMeta>
    This is needed to avoid life time issues between the opened `Array`
    object and the `FragmentInfo` object. FragmentInfo could have copied the
    `FragmentMeta`, however we want to slowly remove all unneeded usages of
    raw pointers. This cause the `FragmentMeta` objects themselves did not
    need to be raw pointers and using a `tdb_shared_ptr` made sense and
    allows us to avoid the copy.

--- tiledb/sm/query/result_tile.cc
+++ tiledb/sm/query/result_tile.cc
@@ -341,7 +341,7 @@ void ResultTile::compute_results_dense(
     const ResultTile* result_tile,
     unsigned dim_idx,
     const Range& range,
-    const std::vector<FragmentMetadata*> fragment_metadata,
+    const std::vector<tdb_shared_ptr<FragmentMetadata>> fragment_metadata,
     unsigned frag_idx,
     std::vector<uint8_t>* result_bitmap,
     std::vector<uint8_t>* overwritten_bitmap) {
@@ -691,7 +691,7 @@ void ResultTile::compute_results_sparse(
 Status ResultTile::compute_results_dense(
     unsigned dim_idx,
     const Range& range,
-    const std::vector<FragmentMetadata*> fragment_metadata,
+    const std::vector<tdb_shared_ptr<FragmentMetadata>> fragment_metadata,
     unsigned frag_idx,
     std::vector<uint8_t>* result_bitmap,
     std::vector<uint8_t>* overwritten_bitmap) const {
